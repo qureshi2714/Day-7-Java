2032735e2b4bcdf12bbdc5a9090e0b0deb0b850b
qureshi2714 <msmuskanshaik65@gmail.com>
2024-12-08 14:56:03 +0530

    Add files via upload

---
+++ Prefix.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Prefix{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int a[]={11,2,3,4,3};
+        int pre[]=new int[a.length];
+        pre[0]=a[0];
+        for(int i=1;i<a.length;i++)
+        pre[i]=pre[i-1]+a[i];
+        System.out.println(Arrays.toString(pre));
+    }
+
+    
+}
